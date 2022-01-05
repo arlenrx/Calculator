227e8fa8ee3617956516b6818d3ce4120f3697a8
Argo <kasymbaevarlen@gmail.com>
2022-01-05 12:50:09 +0600

    First commit

---
+++ src/com/company/ArabianCal.java
@@ -0,0 +1,8 @@
+package com.company;
+import java.util.Scanner;
+public class ArabianCal {
+    static Scanner scanner = new Scanner(System.in);
+    static int number1, number2;
+    static char operation;
+    static int result;
+}
